c93aa7ac97ec5de839b47f95fef1a22355df45de
echappen <elenichappen@gmail.com>
2013-05-11 11:10:14 -0500

    first commit

---
+++ js/master.js
@@ -0,0 +1,22 @@
+ // $(document).ready(function(){
+
+
+
+ // 	$('#portfolio li:first-child').fadeIn(100);
+
+ // 	var interval = setInterval(function(){
+ // 		console.log('start interval');
+ // 		var last_item = $('#portfolio li:visible:last');
+ // 		$(last_item).next().fadeIn("fast");
+ // 		if($(last_item).hasClass('last')) {
+ // 			clearInterval(interval);
+ // 			console.log('stop interval');
+ // 		}
+ // 		// if($('#portfolio li:last')) {
+ // 		// 	alert('last item');
+ // 		// }
+
+ // 	}, 100);
+
+ // });
+
